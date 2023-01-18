b5354f6076609ce2fd174998328eb65c95bae18e
HappyZombie_Slayer <jacob.malland@gmail.com>
2023-01-18 15:09:11 -0600

    Got timer working, somewhat. Needs to remove timestamp

--- script.js
+++ script.js
@@ -49,15 +49,25 @@ function grabQuotes(content) {
 	return(results)
 }
 
-function grabEmotes(content) {
+function grabEmotes(content, quotes) {
 	// Will need to check individually to see if the emoji index is chronologically found within a quote.
 	results = content.match(/<a?:.+?:\d{18}>|\p{Extended_Pictographic}/gu) // Pre ES6 way of parsing emoji's from messages
 	if (results == null) { // No emojis found
 		return([]) // Return an empty list
 	}
 	for (e of results) { // Loop through each emoji found
-		if (!content.includes(e + " \"")) { // Emoji is not represented as a survey option
-			results.splice(results.indexOf(e), 1) // Remove the emoji from the list
+		for (q of quotes) { // Check each quote to see if the emoji is within it
+			emoji = content.indexOf(e) // Store the index of the emoji
+			start = content.indexOf(q)// The start index of the quote
+			end = start + q.length // The end index of the quote
+			if (emoji < start) { // The emoji is found prior to the quote
+				break // Quit the loop
+			}
+			else if (emoji > start && emoji < end) { // The emoji is found within the quote
+				results.splice(results.indexOf(e), 1); // Remove the emoji from the list
+				content = content.substring(emoji + 1) // Skip past the current emoji
+				break // Quit the loop
+			}
 		}
 	}
 	return(results)
@@ -72,7 +82,7 @@ function conductSurvey(message) {
 	}
 
 	quotes = grabQuotes(message.content) // Store all the quotes in the survey
-	emotes = grabEmotes(message.content) // Store all the emojis used in the survey
+	emotes = grabEmotes(message.content, quotes) // Store all the emojis used in the survey
 	
 	times = message.content.substring(message.content.indexOf(quotes[quotes.length - 1]) + quotes[quotes.length - 1].length + 1).trim() // Cut out the time requirement, if it exists
 	
@@ -83,16 +93,29 @@ function conductSurvey(message) {
 		times = [0, 0, 0, 0]
 	}
 
+	// Values Per Unit
 	HOURS = 24
 	MINUTES = 60
 	SECONDS = 60
 
+	// Defeault Values For Each Reset
+	DEFAULT_SECONDS = 60
+	DEFAULT_MINUTES = 60
+	DEFAULT_HOURS = 24
+
+	// Days, Hours, Minutes, & Seconds ~ Time Values
 	d = parseInt(times[0]) // Number of days
 	h = parseInt(times[1]) // Number of hours
 	m = parseInt(times[2]) // Number of minutes
 	s = parseInt(times[3]) // Number of seconds
 
 	timestamp = "<t:" + (parseInt(Date.now()/1000) + (d * HOURS * MINUTES * SECONDS) + (h * MINUTES * SECONDS) + (m * SECONDS) + s) + ":R>" + "<t:" + (parseInt(Date.now()/1000) + (h * MINUTES * SECONDS) + (m * SECONDS) + s) + ":R>" + "<t:" + (parseInt(Date.now()/1000) + (m * SECONDS) + s) + ":R>" + "<t:" + (parseInt(Date.now()/1000) + s) + ":R>"
+
+	// Days, Hours, Minutes, & Seconds ~ Timestamp Countdown Timers
+	dStamp = d == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + (d * HOURS * MINUTES * SECONDS) + (h * MINUTES * SECONDS) + (m * SECONDS) + s) + ":R>"
+	hStamp = h == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + (h * MINUTES * SECONDS) + (m * SECONDS) + s) + ":R>"
+	mStamp = m == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + (m * SECONDS) + s) + ":R>"
+	sStamp = s == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + s) + ":R>"
 	
 	// Same First and Last Index Error !!!
 	for (var i=0; i<quotes.length - emotes.length; i++) { // Add extra spacing
@@ -117,25 +140,82 @@ function conductSurvey(message) {
 
 	message.channel.send({ // Send the formatted reply
 		files: [...message.attachments.values()],
-		content: header + "\n *\t" + body + "\n * Ends In: --:--:--" + timestamp
+		content: header + "\n *\t" + body + "\n * Ends In: " + timestamp
 	}).then((message) => { // Add the options to the survey
 		for (e of emotes) { // Loop through the options
 			if (e != "") { // The emoji is not null
 				message.react(e) // Add the emoji reaction
 			}
 		}
-		s = setTimeout(() => {
+		SEC = setTimeout(() => {
 			// Reset Seconds Timer To 60s
+			var interval = () => {
+				clearStamp = (val, stamp) => {
+					if (val <= 0) {
+						message.edit(message.content.replace(stamp, "")).then((edit) => { // Clear the timestamp
+							message.content = edit.content // Update the message content to reflect the edit
+							console.log("Cleared A Timestamp.")
+						})
+					}
+				}
+				changeSeconds = () => {
+					console.log("Updated Seconds.")
+					temp = "<t:" + parseInt(Date.now()/1000 + DEFAULT_SECONDS) + ":R>" // Temporary timestamp
+					message.edit(message.content.replace(sStamp, temp)).then((edit) => { // Update the message to show T-60 seconds
+						message.content = edit.content // Update the message content to reflect the edit
+					})
+					sStamp = temp // Update sStamp to equal the current secondstamp
+					m -= 1 // Subtract one minute
+				}
+				changeMinutes = () => {
+					if (s != 0) { // Seconds counter is not 0
+						return // Quit the method
+					}
+					console.log("Updated Minutes.")
+					temp = "<t:" + parseInt(Date.now()/1000 + DEFAULT_MINUTES * SECONDS) + ":R>" // Temporary timestamp
+					message.edit(message.content.replace(mStamp, temp)).then((edit) => { // Update the message to show T-60 minutes
+						message.content = edit.content // Update the message content to reflect the edit
+					})
+					mStamp = temp // Update mStamp to equal the current minutestamp
+					h -= 1 // Subtract one hour
+					changeSeconds() // Update the seconds timestamp
+				}
+				changeHours = () => {
+					if (m != 0 || s != 0) { // Minutes or seconds counter is not 0
+						return // Quit the method
+					}
+					console.log("Updated Hours.")
+					temp = "<t:" + parseInt(Date.now()/1000 + DEFAULT_HOURS * MINUTES * SECONDS) + ":R>" // Temporary timestamp
+					message.edit(message.content.replace(hStamp, temp)).then((edit) => { // Update the message to show T-24 hours
+						message.content = edit.content // Update the message content to reflect the edit
+					})
+					hStamp = temp // Update hStamp to equal the current hourstamp
+					d -= 1 // Subtract one day
+					changeMinutes() // Update the minutes timestamp
+				}
+
+				if (m - 1 >= 0) { // 0 Seconds, but remaining minutes 
+					changeSeconds() // Adjust the current seconds display
+				}
+				else if (m - 1 == 0 && h - 1 >= 0) { // 0 Minutes : 0 Seconds, but remaining hours
+					changeMinutes() // Adjust the current minutes display
+				}
+				else if (m - 1 == 0 && h - 1 == 0 && d - 1 >= 0) { // 0 Hours : 0 Minutes : 0 Seconds, but remaining days
+					changeHours() // Adjust the current hours display
+				}
+				else { // 0 Days : 0 Hours : 0 Minutes : 0 Seconds
+					clearInterval(this) // Clear the interval, so it stops updating.
+				}
+
+				clearStamp(s, sStamp) // Clear the secondstamp, if necessary
+				clearStamp(m, mStamp) // Clear the minutestamp, if necessary
+				clearStamp(h, hStamp) // Clear the hourstamp, if necessary
+				clearStamp(d, dStamp) // Clear the daystamp, if necessary
+			}
+			interval() // Run the interval method to update the content.
+			setInterval(interval, SECONDS * 1000) // Set the interval timer to run per minute
+			// NEED SOME WAY TO STOP INTERVAL TIMER
 		}, s * 1000)
-		m = setTimeout(() => {
-			// Reset Minutes Timer To 60m
-		}, (m * SECONDS + s) * 1000)
-		h = setTimeout(() => {
-			// Reset Hours Timer To 24h
-		}, (h * MINUTES * SECONDS + m * SECONDS + s) * 1000)
-		//d = setTimeout(() => {
-		//	// Reset Days Timer (Not Needed)
-		//}, (d * HOURS * MINUTES * SECONDS + h * MINUTES * SECONDS + m * SECONDS + s) * 1000)
 	})
 	.catch(() => {
 		// None ? 
