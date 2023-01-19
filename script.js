6bb62ff3d3108edeb56dab5e9bd30e5de9510715
HappyZombieSlayer <62392847+HappyZombieSlayer@users.noreply.github.com>
2023-01-18 19:35:40 -0600

    New formatting

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ client.once('ready', () => {
 	return;
 });
 
+runningSurveys = [] // List of active survey messages
+concludedSurveys = [] // List of concluded survey messages
+
 function grabQuotes(content) {
 	results = []
 	string = ""
@@ -74,6 +77,7 @@ function grabEmotes(content, quotes) {
 }
 
 function conductSurvey(message) {
+	FORMAT = "F"
 	console.log("Conducting Survey")
 	if (message.content.indexOf("!survey \"") != 0) { // Structure of the command is invalid
 		// Invalid Formatting Error !!!
@@ -84,40 +88,8 @@ function conductSurvey(message) {
 	quotes = grabQuotes(message.content) // Store all the quotes in the survey
 	emotes = grabEmotes(message.content, quotes) // Store all the emojis used in the survey
 	
-	times = message.content.substring(message.content.indexOf(quotes[quotes.length - 1]) + quotes[quotes.length - 1].length + 1).trim() // Cut out the time requirement, if it exists
-	
-	if (times.includes(":")) { // The survey request has a time constraint
-		times = times.split(":")
-	}
-	else { // The survey request lasts for the default amount of time
-		times = [0, 0, 0, 0]
-	}
-
-	// Values Per Unit
-	HOURS = 24
-	MINUTES = 60
-	SECONDS = 60
-
-	// Defeault Values For Each Reset
-	DEFAULT_SECONDS = 60
-	DEFAULT_MINUTES = 60
-	DEFAULT_HOURS = 24
-
-	// Days, Hours, Minutes, & Seconds ~ Time Values
-	d = parseInt(times[0]) // Number of days
-	h = parseInt(times[1]) // Number of hours
-	m = parseInt(times[2]) // Number of minutes
-	s = parseInt(times[3]) // Number of seconds
-
-	// Days, Hours, Minutes, & Seconds ~ Timestamp Countdown Timers
-	dStamp = d == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + (d * HOURS * MINUTES * SECONDS) + (h * MINUTES * SECONDS) + (m * SECONDS) + s) + ":R>"
-	hStamp = h == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + (h * MINUTES * SECONDS) + (m * SECONDS) + s) + ":R>"
-	mStamp = m == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + (m * SECONDS) + s) + ":R>"
-	sStamp = s == 0 ? "" : "<t:" + (parseInt(Date.now()/1000) + s) + ":R>"
-	
-	timestamp = dStamp + hStamp + mStamp + sStamp
-
-	console.log(timestamp)
+	minutes = 60 * parseInt(message.content.substring(message.content.indexOf(quotes[quotes.length - 1]) + quotes[quotes.length - 1].length + 1).trim()) // Cut out the time requirement, if it exists
+	timestamp = "<t:" + parseInt(Date.now()/1000 + minutes) + ":" + FORMAT + ">" // Creates the timestamp to signal the end of the survey duration
 
 	// Same First and Last Index Error !!!
 	for (var i=0; i<quotes.length - emotes.length; i++) { // Add extra spacing
@@ -126,7 +98,6 @@ function conductSurvey(message) {
 
 	console.log(quotes)
 	console.log(emotes)
-	console.log(d + " Days. " + h + " Hours. " + m + " Mins. " + s + " Secs.")
 
 	header = "" // Stores the heading of the formatted reply
 	body = "" // Stores the body of formatted emojis and options
@@ -142,110 +113,19 @@ function conductSurvey(message) {
 
 	message.channel.send({ // Send the formatted reply
 		files: [...message.attachments.values()],
-		content: header + "\n *\t" + body + "\n * Ends In: " + timestamp
+		content: header + "\n *\t" + body + "\n * \n * Ends At: " + timestamp
 	}).then((message) => { // Add the options to the survey
 		for (e of emotes) { // Loop through the options
 			if (e != "") { // The emoji is not null
 				message.react(e) // Add the emoji reaction
 			}
 		}
-		SEC = setTimeout(() => {
-			// Reset Seconds Timer To 60s
-			var interval = () => {
-				clearStamp = (val, stamp) => {
-					if (val <= 0 && stamp != "") {
-						message.edit(message.content.replace(stamp, "")).then((edit) => { // Clear the timestamp
-							message.content = edit.content // Update the message content to reflect the edit
-						})
-						return(true)
-					}
-					return(false)
-				}
-				updateSeconds = (DEFAULT) => {
-					temp = "<t:" + parseInt(Date.now()/1000 + DEFAULT) + ":R>"
-					message.edit(message.content.replace(sStamp, temp)).then((edit) => { // Update the message to show T-60 seconds
-						message.content = edit.content // Update the message content to reflect the edit
-					})
-					sStamp = temp // Update the secondstamp
-					return(DEFAULT) // Return the default value
-				}
-				updateMinutes = (DEFAULT) => {
-					temp = "<t:" + parseInt(Date.now()/1000 + DEFAULT * SECONDS) + ":R>"
-					message.edit(message.content.replace(mStamp, temp)).then((edit) => { // Update the message to show T-60 seconds
-						message.content = edit.content // Update the message content to reflect the edit
-					})
-					mStamp = temp // Update the minutestamp
-					return(DEFAULT) // Return the default value
-				}
-				updateHours = (DEFAULT) => {
-					temp = "<t:" + parseInt(Date.now()/1000 + DEFAULT * MINUTES * SECONDS) + ":R>"
-					message.edit(message.content.replace(hStamp, temp)).then((edit) => { // Update the message to show T-60 seconds
-						message.content = edit.content // Update the message content to reflect the edit
-					})
-					hStamp = temp // Update the hourstamp
-					return(DEFAULT) // Return the default value
-				}
-				changeSeconds = () => {
-					console.log("Updated Seconds.")
-					m -= 1 // Subtract one minute
-					s = updateSeconds(DEFAULT_SECONDS) // Set 's' to default value
-				}
-				changeMinutes = () => {
-					if (s != 0) { // Seconds counter is not 0
-						return // Quit the method
-					}
-					console.log("Updated Minutes.")
-					h -= 1 // Subtract one hour
-					m = updateMinutes(DEFAULT_MINUTES) // Set 'm' to default value
-					changeSeconds() // Update the seconds timestamp
-				}
-				changeHours = () => {
-					if (m != 0 || s != 0) { // Minutes or seconds counter is not 0
-						return // Quit the method
-					}
-					console.log("Updated Hours.")
-					d -= 1 // Subtract one day
-					h = updateHours(DEFAULT_HOURS) // Set 'h' to default value
-					changeMinutes() // Update the minutes timestamp
-				}
-
-				if (m - 1 >= 0) { // 0 Seconds, but remaining minutes 
-					changeSeconds() // Adjust the current seconds display
-				}
-				else if (m - 1 == 0 && h - 1 >= 0) { // 0 Minutes : 0 Seconds, but remaining hours
-					changeMinutes() // Adjust the current minutes display
-				}
-				else if (m - 1 == 0 && h - 1 == 0 && d - 1 >= 0) { // 0 Hours : 0 Minutes : 0 Seconds, but remaining days
-					changeHours() // Adjust the current hours display
-				}
-				else if (s == 0 && m == 0 && h == 0 && d == 0) { // 0 Days : 0 Hours : 0 Minutes : 0 Seconds
-					message.edit(message.content.substring(0, message.content.indexOf("\n * Ends In: ")) + "\n * Ends In: ** Survey Has Concluded").then((edit) => {
-						message.content = edit.content // Update message content
-					})
-					clearInterval(this) // Clear the interval, so it stops updating.
-				}
-
-				if (clearStamp(s, sStamp)) { // Clear the secondstamp, if necessary
-					console.log("Cleared Secondstamp.")
-					sStamp = ""
-				}
-				if (clearStamp(m, mStamp)) {
-					console.log("Cleared Minutestamp.")
-					mStamp = ""
-					updateSeconds(s)
-				}
-				if (clearStamp(h, hStamp)) {
-					console.log("Cleared Hourstamp.")
-					hStamp = ""
-					updateMinutes(m)
-					updateSeconds(s)
-				}
-				dStamp = clearStamp(d, dStamp) ? "" : dStamp // Clear the daystamp, if necessary
-			}
-			interval() // Run the interval method to update the content.
-			setInterval(interval, SECONDS * 1000) // Set the interval timer to run per minute
-			// NEED SOME WAY TO STOP INTERVAL TIMER
-		}, s * 1000)
+		setTimeout(() => {
+			message.edit(message.content.substring(0, message.content.indexOf("\n * Ends At: ")) + "\n * Ends At: ** Survey Has Concluded").then(() => {
+				runningSurveys.remove(runningSurveys.indexOf(message)) // Remove the message from the running surveys
+				concludedSurveys.push(message) // Add the message to the concluded surveys
+			})
+		}, minutes * 60 * 1000)
 	})
 	.catch(() => {
 		// None ? 
@@ -259,6 +139,15 @@ client.on("messageCreate", async (message) => {
 	console.log(message.content)
 	if (message.content.charAt(0) == '!') {
 		conductSurvey(message)
+		runningSurveys.push(message)
+	}
+})
+
+client.on("messageReactionAdd", async (reaction) => {
+	console.log("Reaction Added")
+	if (reaction.message in concludedSurveys) { // The survey has been concluded
+		reaction.remove() // Remove the reaction
+		console.log("Reaction Removed")
 	}
 })
 
